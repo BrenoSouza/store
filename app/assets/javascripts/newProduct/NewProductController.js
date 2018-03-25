690e573828af22cb6caac93290a301fc92f806e2
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-03-25 18:42:38 -0300

    delete action

--- app/assets/javascripts/newProduct/NewProductController.js
+++ app/assets/javascripts/newProduct/NewProductController.js
@@ -10,6 +10,10 @@ function NewProductController($scope, $http, $state, ProductFactory) {
             $scope.newProduct.dislikes = 0;
             $scope.newProduct.category = document.getElementById('category').value;
             
+            if ($scope.newProduct.url.length < 5) {
+                $scope.newProduct.url = 'http://placehold.it/120x160';
+            }
+
             ProductFactory.createProduct($scope.newProduct)
             .then(function (response) {
                 console.log(response)
